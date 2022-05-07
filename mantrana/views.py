e85e75ac7ca88370648de0bf5a7fa83065335549
Ingenious Abhinav <104002709+ingeniousabhinavvv@users.noreply.github.com>
2022-05-07 23:30:03 +0530

    First Commit
    Litte Update

--- mantrana/views.py
+++ mantrana/views.py
@@ -1,3 +1,7 @@
+import imp
+from pkgutil import ImpImporter
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
